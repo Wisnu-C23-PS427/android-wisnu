3b26b31c2832e3b66e6b3faaec14c3a113d2668c
Rzldo <ezas4531@gmail.com>
2023-05-24 22:49:01 +0700

    slicing UI login,wellcome and navigation

--- app/src/main/java/space/mrandika/wisnu/presentation/welcome/WelcomeFragment.kt
+++ app/src/main/java/space/mrandika/wisnu/presentation/welcome/WelcomeFragment.kt
@@ -1,34 +1,31 @@
-package space.mrandika.wisnu.presentation.splash
+package space.mrandika.wisnu.presentation.welcome
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import space.mrandika.wisnu.R
-import space.mrandika.wisnu.databinding.FragmentSplashBinding
 import space.mrandika.wisnu.databinding.FragmentWelcomeBinding
 
-class SplashFragment : Fragment() {
-    private var _binding : FragmentSplashBinding? = null
-    private val binding : FragmentSplashBinding
+class WelcomeFragment : Fragment() {
+    private var _binding : FragmentWelcomeBinding? = null
+    private val binding : FragmentWelcomeBinding
         get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSplashBinding.inflate(layoutInflater, container,false)
-        return  binding.root
+        _binding = FragmentWelcomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Handler(Looper.getMainLooper()).postDelayed({
-            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToWelcomeFragment())
-        },3000)
+        binding.btnLogin.setOnClickListener{
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment())
+        }
     }
+
 }
