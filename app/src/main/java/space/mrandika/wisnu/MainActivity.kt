e3026d916022991f2dfd6c9c715640f15e6c9bfa
Muhammad Rizki Andika <Mrizkiandika226@gmail.com>
2023-05-24 18:23:00 +0700

    Init project

---
+++ app/src/main/java/space/mrandika/wisnu/MainActivity.kt
@@ -0,0 +1,20 @@
+package space.mrandika.wisnu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import space.mrandika.wisnu.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    /**
+     * ViewBinding
+     */
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
