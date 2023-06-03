785487abd92ef413e8640dd016427d4cb1d1a7f2
Rzldo <ezas4531@gmail.com>
2023-06-04 02:52:50 +0700

    add card

--- app/src/main/java/space/mrandika/wisnu/ui/poi/poidetail/PoiDetailUiState.kt
+++ app/src/main/java/space/mrandika/wisnu/ui/poi/poidetail/PoiDetailUiState.kt
@@ -1,9 +1,12 @@
 package space.mrandika.wisnu.ui.poi.poidetail
 
+import space.mrandika.wisnu.model.guide.Guide
 import space.mrandika.wisnu.model.poi.POIResponse
 
 data class PoiDetailUiState (
     val isLoading : Boolean = false,
     val isError : Boolean = false,
-    val DetailResult : POIResponse? = null
-        )
+    val isEmpty : Boolean = false,
+    val DetailResult : POIResponse? = null,
+    val GuideResult : Guide? = null
+)
--- app/src/main/java/space/mrandika/wisnu/ui/poi/poidetail/PoiDetailViewModel.kt
+++ app/src/main/java/space/mrandika/wisnu/ui/poi/poidetail/PoiDetailViewModel.kt
@@ -5,40 +5,71 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import space.mrandika.wisnu.model.guide.Guide
 import space.mrandika.wisnu.model.poi.POIResponse
+import space.mrandika.wisnu.repository.GuideRepository
 import space.mrandika.wisnu.repository.POIRepository
+import javax.inject.Inject
 
 @HiltViewModel
-class PoiDetailViewModel(private val repo : POIRepository) :ViewModel(){
+class PoiDetailViewModel @Inject constructor(private val repoPOI : POIRepository, private val repoGuide : GuideRepository) :ViewModel(){
     private val _state = MutableStateFlow(PoiDetailUiState())
     val state : StateFlow<PoiDetailUiState> = _state
 
     suspend fun getDetailPoi(id: Int){
         isLoading(true)
         isError(false)
-        repo.getPOI(id).collect{ result ->
+        repoPOI.getPOI(id).collect{ result ->
             isLoading(false)
             result.onFailure {
-                isError(value = false)
+                isError(value = true)
             }
             result.onSuccess {
                 isSuccess(value = it)
             }
         }
     }
-    fun isLoading(value: Boolean){
+    suspend fun getGuide(id:Int){
+        isLoading(true)
+        isError(false)
+        repoGuide.getGuide(id).collect{ result ->
+            isLoading(false)
+            result.onFailure {
+                isError(value = true)
+            }
+            result.onSuccess {
+                if(it.data == null ){
+                    isEmpty(true)
+                }else{
+                    getGuideSuccess(it.data)
+                }
+            }
+        }
+    }
+    private fun isLoading(value: Boolean){
         _state.update {
             it.copy(isLoading = value)
         }
     }
-    fun isError(value: Boolean){
+    private fun isError(value: Boolean){
         _state.update {
             it.copy(isError = value)
         }
     }
-    fun isSuccess(value : POIResponse){
+    private fun isSuccess(value : POIResponse){
         _state.update {
             it.copy(DetailResult = value)
+        }
+    }
+
+    private fun isEmpty(value: Boolean){
+        _state.update {
+            it.copy(isEmpty = value)
+        }
+    }
+    private fun getGuideSuccess(value:Guide){
+        _state.update {
+            it.copy(GuideResult = value)
         }
     }
 }
