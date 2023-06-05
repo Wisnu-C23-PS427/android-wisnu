6d61436a0f9102654cde635d50d1afbc873443ad
Muhammad Rizki Andika <Mrizkiandika226@gmail.com>
2023-06-05 22:06:43 +0700

    Revert MainActivity

--- app/src/main/java/space/mrandika/wisnu/MainActivity.kt
+++ app/src/main/java/space/mrandika/wisnu/MainActivity.kt
@@ -14,8 +14,6 @@ class MainActivity : AppCompatActivity() {
      */
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: SearchViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
